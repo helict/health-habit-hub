67c9ecb1a8e7074147b73e4056674b7d252eb57c
Johannes Zeller <mail@johannes-zeller.net>
2024-05-17 12:19:05 +0200

    Refactor highlighting function

--- app/public/js/donate.js
+++ app/public/js/donate.js
@@ -1,6 +1,7 @@
 function markSelection(context, editableId) {
-  let editable = document.getElementById(editableId);
-  let selection = window.getSelection();
+  const editable = document.getElementById(editableId);
+  const selection = window.getSelection();
+
   if (
     !editable.contains(selection.anchorNode) ||
     !editable.contains(selection.focusNode)
@@ -8,69 +9,51 @@ function markSelection(context, editableId) {
     // Selection is not exclusively in the editable div
     return;
   }
-  // Wrap selected text in a span
-  let mark = document.createElement("mark");
-  mark.className = `mark_${context}`;
-  range = selection.getRangeAt(0);
 
-  if (range.startContainer.parentNode.nodeName === "MARK") {
-    // Cannot use setStartAfter because https://issues.chromium.org/issues/41239578
-    range.setStart(range.startContainer.parentNode.nextSibling, 0);
-  }
-  if (range.endContainer.parentNode.nodeName === "MARK") {
-    // Cannot use setEndBefore because https://issues.chromium.org/issues/41239578
-    range.setEnd(
-      range.endContainer.parentNode.previousSibling,
-      range.endContainer.parentNode.previousSibling.length
+  const range = selection.getRangeAt(0);
+  const selectedText = range.toString();
+
+  // Truncate overlapping highlights
+  const startParent = range.startContainer.parentNode;
+  if (startParent !== editable) {
+    startParent.textContent = startParent.textContent.substring(
+      0,
+      range.startOffset
     );
+    // Cannot use setStartAfter because https://issues.chromium.org/issues/41239578
+    range.setStart(startParent.nextSibling, 0);
   }
-  if (
-    range.startContainer === range.endContainer &&
-    range.startContainer.parentNode === editable
-  ) {
-    range.surroundContents(mark);
-    cleanUp(editable);
-  }
-  console.log(editable.innerHTML);
-}
 
-function cleanUp(editable) {
-  // merge all child nodes of editable which are identical if they immediately follow each other
-  // remove empty text nodes
-  removeEmptyTextNodes(editable);
-  let children = editable.childNodes;
-  let i = 0;
-  while (i < children.length) {
-    let current = children[i];
-    let next = children[i + 1];
-    console.log(current, next);
-    if (
-      next &&
-      current.nodeName === next.nodeName &&
-      current.className === next.className
-    ) {
-      current.append(next.innerHTML);
-      next.remove();
-    } else {
-      i++;
-    }
+  const endParent = range.endContainer.parentNode;
+  if (endParent !== editable) {
+    endParent.textContent = endParent.textContent.substring(range.endOffset);
+    // Cannot use setEndBefore because https://issues.chromium.org/issues/41239578
+    range.setEnd(endParent.previousSibling, endParent.previousSibling.length);
   }
-}
 
-function removeEmptyTextNodes(editable) {
-  let children = editable.childNodes;
-  for (let i = 0; i < children.length; i++) {
-    let current = children[i];
-    if (
-      current.nodeType === Node.TEXT_NODE &&
-      current.textContent.trim() === ""
-    ) {
-      current.remove();
-    }
-  }
+  // Create new mark element
+  const mark = document.createElement("mark");
+  mark.className = `mark_${context}`;
+  mark.textContent = selectedText;
+  range.deleteContents();
+  range.insertNode(mark);
 }
 
 function removeAllHighlights(editableId) {
   let editable = document.getElementById(editableId);
   editable.innerHTML = editable.innerText;
+}
+
+// Ensure that only first-level mark elements are present in the editable div
+function cleanUpEditable(editableID) {
+  const editable = document.getElementById(editableID);
+  // remove all markup other than first-level mark elements
+  editable.innerHTML = DOMPurify.sanitize(editable.innerHTML, {
+    ALLOWED_TAGS: ["mark"],
+    ALLOWED_ATTR: ["class"],
+  });
+
+  for (const child of editable.children) {
+    child.innerHTML = child.textContent;
+  }
 }
