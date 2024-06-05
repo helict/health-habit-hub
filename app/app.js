e3e132cc1b2c2ea49e3e6a0d2423e7d5a7a5327e
pe5kataa <petar.nikodimov@gmail.com>
2024-06-05 15:59:37 +0200

    Use the new class from SparqlDatabase.js to construct the client config
    Signed-off-by: pe5kataa <petar.nikodimov@gmail.com>

--- app/app.js
+++ app/app.js
@@ -1,15 +1,11 @@
 import express from 'express';
-import sparqlClient from 'sparql-http-client';
-import { v4 as uuid } from 'uuid';
+import {config} from './utils/config.js';
+import {staticFileMiddleware} from './middleware/staticFileMiddleware.js';
+import {jsonBodyParser} from './middleware/requestParser.js';
+import {SparqlClient} from './utils/SparqlDatabase.js';
 
-import { translate } from 'deeplx';
-
-import { config } from './utils/config.js';
-import { staticFileMiddleware } from './middleware/staticFileMiddleware.js';
-import { jsonBodyParser } from './middleware/requestParser.js';
 
 // Express config
-
 import donateRouter from './routes/donateRouter.js';
 import aboutRouter from './routes/aboutRouter.js';
 
@@ -17,10 +13,7 @@ const app = express();
 const port = config.port;
 
 // SPARQL client config
-const db_user = config.db.user;
-const db_pass = config.db.password;
-const db_endpoint = config.getDbEndpoint();
-const db_headers = config.getDbHeader();
+const sparqlClient = new SparqlClient();
 
 // Middleware to parse form data in the request body
 app.use(jsonBodyParser);
@@ -42,123 +35,6 @@ app.use('/donate', donateRouter);
 app.use('/about', aboutRouter);
 
 /* eslint-disable */
-//SPARQL Connection
-async function insertDataClosed() {
-  const keys = Array.from(selectionDataClosed.keys());
-  console.log('Keys:', keys, keys.length);
-  console.log('Inserting closed data into', db_endpoint);
-  const habituuid = uuid.v4();
-  const client = new sparqlClient({
-    updateUrl: db_endpoint,
-    user: db_user,
-    password: db_pass,
-    headers: db_headers,
-  });
-
-  let closedQuery = `
-  PREFIX hhh: <http://example.com/hhh#>
-  PREFIX owl: <http://www.w3.org/2002/07/owl#>
-  PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-  PREFIX xml: <http://www.w3.org/XML/1998/namespace>
-  PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
-  PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-  BASE <http://www.w3.org/2002/07/owl#>
-
-  INSERT DATA {
-    hhh:ExperimentalSetting-${habituuid} rdf:type owl:NamedIndividual,
-                                          hhh:${group_sql}.
-                   
-  `;
-
-  //this adds the data according to user input to the query
-  let i = 0;
-  while (i < keys.length) {
-    if (i == keys.length - 1) {
-      closedQuery += `
-      hhh:Behaviour-${habituuid} rdf:type owl:NamedIndividual,
-                    hhh:${keys[i]} ; 
-                              hhh:partOf hhh:ExperimentalSetting-${habituuid};
-                              hhh:id "${habituuid}" ;
-                              hhh:language "${dataLanguage}"^^rdf:langString;
-                              hhh:source "${inputSource}"^^rdfs:Literal;
-                              hhh:value "${await translate(
-                                selectionDataClosed.get(keys[i]),
-                                'en',
-                                dataLanguage,
-                              )}".
-                              `;
-      break;
-    } else {
-      closedQuery += `
-        hhh:Behaviour-${habituuid} rdf:type owl:NamedIndividual,
-                    hhh:${keys[i]} ; 
-                              hhh:partOf hhh:ExperimentalSetting-${habituuid};
-                              hhh:id "${habituuid}" ;
-                              hhh:language "${dataLanguage}"^^rdf:langString;
-                              hhh:source "${inputSource}"^^rdfs:Literal;
-                              hhh:value "${await translate(
-                                selectionDataClosed.get(keys[i]),
-                                'en',
-                                dataLanguage,
-                              )}".
-                              `;
-    }
-    i++;
-  }
-
-  closedQuery += `}`;
-  try {
-    await client.query.update(closedQuery);
-    console.log('Data inserted successfully uuid:', habituuid);
-  } catch (error) {
-    console.debug(closedQuery);
-    console.error('Error inserting data:', error.message);
-  }
-}
-
-async function insertDataOpen() {
-  console.log('Inserting open data into', db_endpoint);
-  const habituuid = uuid.v4();
-  const client = new sparqlClient({
-    updateUrl: db_endpoint,
-    user: db_user,
-    password: db_pass,
-    headers: db_headers,
-  });
-  let openQuery = `
-  PREFIX hhh: <http://example.com/hhh#>
-  PREFIX owl: <http://www.w3.org/2002/07/owl#>
-  PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-  PREFIX xml: <http://www.w3.org/XML/1998/namespace>
-  PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
-  PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-  BASE <http://www.w3.org/2002/07/owl#>
-
-  INSERT DATA {
-    hhh:ExperimentalSetting-${habituuid} rdf:type owl:NamedIndividual,
-                                          hhh:${group_sql}.
-
-    hhh:Behaviour-${habituuid} rdf:type owl:NamedIndividual,
-                hhh:Behaviour ; 
-                          hhh:partOf hhh:ExperimentalSetting-${habituuid};
-                          hhh:id "${habituuid}" ;
-                          hhh:language "${dataLanguage}"^^rdf:langString;
-                          hhh:source "${inputSource}"^^rdfs:Literal;
-                          hhh:value "${await translate(
-                            dataOpen,
-                            'en',
-                            dataLanguage,
-                          )}".
-    }`;
-
-  try {
-    await client.query.update(openQuery);
-    console.log('Data inserted successfully uuid:', habituuid);
-  } catch (error) {
-    console.debug(openQuery);
-    console.error('Error inserting data:', error.message);
-  }
-}
 
 app.listen(port, () => {
   console.log(`Server is running on http://app.localhost`);
