5966f1dc048df9bc56730be6c781705c69b795d8
pe5kataa <petar.nikodimov@gmail.com>
2024-07-03 15:27:45 +0200

    Context config created

---
+++ app/utils/contexts.js
@@ -0,0 +1,27 @@
+export const contexts = [
+  {
+    "id": "physical",
+    "label": "Physical",
+    "color": "#00ff00"
+  },
+  {
+    "id": "prior",
+    "label": "Prior",
+    "color": "#0000ff"
+  },
+  {
+    "id": "people",
+    "label": "People",
+    "color": "#ffff00"
+  },
+  {
+    "id": "internal",
+    "label": "Internal",
+    "color": "#ff00ff"
+  },
+  {
+    "id": "behavior",
+    "label": "Behavior",
+    "color": "#00ffff"
+  }
+];
