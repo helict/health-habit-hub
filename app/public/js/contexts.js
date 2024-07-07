a3aef27da06ecbbc06b082f79993ce56f2b32cad
pe5kataa <petar.nikodimov@gmail.com>
2024-07-07 22:04:55 +0200

    Add different languages for the contexts

--- app/public/js/contexts.js
+++ app/public/js/contexts.js
@@ -1,27 +1,47 @@
 export const contexts = [
   {
-    "id": "physical",
-    "label": "Physical",
-    "color": "#b2d8b2" // Light green
+    id: "physical",
+    labels: {
+      en: "Physical",
+      de: "Physisch",
+      ja: "物理的"
+    },
+    color: "#b2d8b2"
   },
   {
-    "id": "prior",
-    "label": "Prior",
-    "color": "#b2c7e1" // Light blue
+    id: "prior",
+    labels: {
+      en: "Prior",
+      de: "Vorher",
+      ja: "前"
+    },
+    color: "#b2c7e1"
   },
   {
-    "id": "people",
-    "label": "People",
-    "color": "#fff8b2" // Light yellow
+    id: "people",
+    labels: {
+      en: "People",
+      de: "Menschen",
+      ja: "人々"
+    },
+    color: "#fff8b2"
   },
   {
-    "id": "internal",
-    "label": "Internal",
-    "color": "#e1b2e1" // Light lavender
+    id: "internal",
+    labels: {
+      en: "Internal",
+      de: "Intern",
+      ja: "内部の"
+    },
+    color: "#e1b2e1"
   },
   {
-    "id": "behavior",
-    "label": "Behavior",
-    "color": "#b2e1e1" // Light teal
+    id: "behavior",
+    labels: {
+      en: "Behavior",
+      de: "Verhalten",
+      ja: "行動"
+    },
+    color: "#b2e1e1"
   }
 ];
