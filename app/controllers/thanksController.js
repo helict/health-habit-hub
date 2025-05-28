38ca20b4939dfdd08506fa72548e37afa892df36
Bulgy404 <felixreinsch@felixreinsch.de>
2025-05-28 10:36:30 +0200

    cleaned thank you page

--- app/controllers/thanksController.js
+++ app/controllers/thanksController.js
@@ -1,10 +1,23 @@
 import url from 'url';
 import { getLanguageMessages } from '../utils/localization.js';
 
-// Funktion zum Rendern der About-Seite
 export function renderThanksPage(req, res) {
+  // grab a shallow copy so we don’t mutate the shared object
+  const messages = { ...getLanguageMessages(req.lang) };
+
+  // helper to wrap ALL-CAPS words
+  const wrapCaps = str =>
+    str.replace(/\b([A-Z]{2,})\b/g, '<span class="caps-green">$1</span>');
+
+  // apply to both thanks and slogan if they exist
+  ['thanks', 'slogan'].forEach(key => {
+    if (typeof messages[key] === 'string') {
+      messages[key] = wrapCaps(messages[key]);
+    }
+  });
+
   res.render(
     url.fileURLToPath(new URL('../views/thanks.ejs', import.meta.url)),
-    getLanguageMessages(req.lang)
+    messages
   );
 }
