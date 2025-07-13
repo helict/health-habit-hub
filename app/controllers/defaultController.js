08108847bc40469458370428dc10ed7bfb085413
Felix <115490923+Bulgy404@users.noreply.github.com>
2025-07-13 19:21:39 +0200

    fixed backtick

--- app/controllers/defaultController.js
+++ app/controllers/defaultController.js
@@ -4,7 +4,7 @@ import { getLanguageMessages } from '../utils/localization.js';
 export function renderLocalizedView(req, res, pageName, extraData = {}) {
   const messages = getLanguageMessages(req.lang);
 
-  const pageUrl = new URL('../views/${pageName}.ejs', import.meta.url);
+  const pageUrl = new URL(`../views/${pageName}.ejs`, import.meta.url);
   const pagePath = url.fileURLToPath(pageUrl);
 
   const data = {
