da7916625268187c09435747c53f261f9c83326e
Jingting-Hua <3130575430@qq.com>
2025-12-31 15:03:30 +0100

    Add /theories/prompt endpoint for behavior change theories (COM-B/TTM/SCT) + fallback

--- API-service/src/openapi_server/main.py
+++ API-service/src/openapi_server/main.py
@@ -18,6 +18,7 @@ from openapi_server.apis.system_api import router as SystemApiRouter
 from openapi_server.apis.profiles_build_api import router as ProfilesBuildApiRouter
 from openapi_server.apis.kb_query_api import router as KbQueryRouter
 from openapi_server.apis.select_habits_api import router as HabitDbSelectRouter
+from openapi_server.apis.theory_prompt_api import router as TheoryPromptApiRouter
 
 WORKFLOW1_TAG = "Workflow1: Habitual structured collection workflow"
 WORKFLOW2_TAG = "Workflow2: User Profile/Form Workflow"
@@ -63,6 +64,7 @@ force_single_tag(BcioMappingApiRouter, WORKFLOW1_TAG)
 force_single_tag(ProfilesBuildApiRouter, WORKFLOW3_TAG)
 force_single_tag(HabitDbSelectRouter, WORKFLOW3_TAG)
 force_single_tag(KbQueryRouter, WORKFLOW3_TAG)
+force_single_tag(TheoryPromptApiRouter, WORKFLOW3_TAG)
 
 force_single_tag(SystemApiRouter, SYSTEM_TAG)
 
@@ -74,7 +76,7 @@ workflow1_router.include_router(BcioMappingApiRouter)
 workflow3_router.include_router(ProfilesBuildApiRouter)
 workflow3_router.include_router(HabitDbSelectRouter)
 workflow3_router.include_router(KbQueryRouter)
-
+workflow3_router.include_router(TheoryPromptApiRouter)
 
 system_router.include_router(SystemApiRouter)
 
